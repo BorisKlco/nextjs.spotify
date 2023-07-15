3ccb12fcb81509c63787b00ffbcde898089e3130
Boris Klco <klco@pm.me>
2023-07-15 19:10:30 +0200

    Upload Form Creation

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,24 +1,41 @@
 "use client";
 import useUploadModal from "@/app/(hooks)/useUploadModal";
 import Modal from "./Modal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 export default function UploadModal() {
   const uploadModal = useUploadModal();
 
+  const { register, reset, handleSubmit } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
   const onChange = (open: boolean) => {
     if (!open) {
+      reset();
       uploadModal.onClose();
     }
   };
 
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    //handle subabase uploas
+  };
+
   return (
     <Modal
-      title="Upload"
-      description="Upload Modal"
+      title="Add Song"
+      description="Upload MP3 file."
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      Upload Modal
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input />
+      </form>
     </Modal>
   );
 }
